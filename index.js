568fd0eadc1393b3481cffde22f3d2c84ec0f597
Jack O'Nolan <onolan.j@gmail.com>
2018-03-21 15:41:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase === ('hello'))
   return ('I can\'t hear you!')
-} else if { 
-  (string.toUpperCase ===('HELLO')) 
+} if {
+  (string.toUpperCase ===('HELLO'))
   return ('YES INDEED!')
-} else if { 
+} else if {
   (string = ('I love you, Grandma.')
   return ('I love you, too.')
 }
