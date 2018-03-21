f7b77387fcb36949de01c9fdf5420c6e8a3ba201
Jack O'Nolan <onolan.j@gmail.com>
2018-03-21 13:04:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+  return `$(spy)`
+}
+
+function logWhisper(string) {
+  console.log('hello')
+  return `$(spy)`
+}
+
+function sayHiToGrandma(string) {
+  console.log `$('hello!')`
+  return ('I can\'t hear you!')
+}
+
+function sayHiToGrandma(string) {
+  string.toUpperCase()
+  console.log("HELLO!")
+  return ('YES INDEED!')
+}
