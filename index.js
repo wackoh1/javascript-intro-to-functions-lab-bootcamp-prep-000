3a7488ecb45d619673243ce4febee0c1d4db0c6b
Jack O'Nolan <onolan.j@gmail.com>
2018-03-21 15:17:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if ('hello') = lowercase
+  if 'hello'.lowercase
   return ('I can\'t hear you!')
 } else {
   } return ('YES INDEED!')
