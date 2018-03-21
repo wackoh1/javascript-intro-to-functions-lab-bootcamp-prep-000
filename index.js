24e6b8aeebd1286ff1e642dd49756e036c36a02e
Jack O'Nolan <onolan.j@gmail.com>
2018-03-21 16:24:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === uppercase)
+  if (string.toLowerCase('hello') === lowercase)
   return ('I can\'t hear you!')
 
 }
